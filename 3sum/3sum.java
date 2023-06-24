bc7448a4b7820e55b8a723d7d483acd9fd4042af
Tasnim <92645626+tasnimMoh@users.noreply.github.com>
2023-06-25 00:39:05 +0300

    Time: 42 ms (73.28%) | Memory: 50.5 MB (67.00%) - LeetSync

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,30 +1,25 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-
-        // Sort the array
         Arrays.sort(nums);
 
-        // Iterate over the array
+
         for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicates
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
-            int target = -nums[i]; // Find the complement
-
+            int target = -nums[i]; 
             int left = i + 1;
             int right = nums.length - 1;
 
-            // Two-pointers approach
+
             while (left < right) {
                 int sum = nums[left] + nums[right];
 
                 if (sum == target) {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    // Skip duplicates
                     while (left < right && nums[left] == nums[left + 1]) {
                         left++;
                     }
