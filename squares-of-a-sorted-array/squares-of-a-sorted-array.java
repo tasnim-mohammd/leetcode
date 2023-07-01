9f6f9fbc5d4cb5250bc3a88de5ef0f4ece863a10
Tasnim <92645626+tasnimMoh@users.noreply.github.com>
2023-07-01 19:03:06 +0300

    Time: 5 ms (47.78%) | Memory: 44.9 MB (55.22%) - LeetSync

--- squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -7,18 +7,7 @@ class Solution
         {
         nums[i] = (int)Math.pow(nums[i],2);
         }
-        for(int i =0 ; i < nums.length-1 ; i++)
-        {
-            for(int j = i+1; j < nums.length; j++)
-            {
-                if(nums[i] > nums[j])
-                {
-                    temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j]= temp;
-                }
-            }
-        }
+        Arrays.sort(nums);
         return nums;
     }
     
