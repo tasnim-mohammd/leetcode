545ce9f58fb1c8d66fd4967f6cc6fdd078b6f8e2
Tasnim <92645626+tasnimMoh@users.noreply.github.com>
2023-07-05 11:26:14 +0300

    Time: 3 ms (97.20%) | Memory: 47.5 MB (85.21%) - LeetSync

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -1,24 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid.length == 0) 
-        return 0;
-        int count = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    count++;
-                    DepthFirstSearch(grid, i, j);
-                }
-            }
-        }
-        return count;       
+             if(grid.length == 0)
+             return 0;
+             int count = 0;
+             for( int i = 0; i<grid.length;i++)
+             {
+                 for(int j = 0; j< grid[0].length;j++)
+                 {
+                     if(grid[i][j] == '1')
+                     {
+                         count++;
+                         DFS(grid,i,j);
+                     }
+                 }
+             } 
+    return count;
     }
-    private void DepthFirstSearch(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') return;
-        grid[i][j] = '0';
-        DepthFirstSearch(grid, i + 1, j);
-        DepthFirstSearch(grid, i - 1, j);
-        DepthFirstSearch(grid, i, j + 1);
-        DepthFirstSearch(grid, i, j - 1);
+
+    private void DFS(char[][] grid, int i, int j)
+    {
+        if (i<0|| j<0|| i>=grid.length || j>= grid[0].length|| grid[i][j] != '1') return;
+        grid[i][j]='0';
+        DFS(grid,i+1,j);
+        DFS(grid,i-1,j);
+        DFS(grid,i,j+1);
+        DFS(grid,i,j-1);
+        return;
     }
 }
