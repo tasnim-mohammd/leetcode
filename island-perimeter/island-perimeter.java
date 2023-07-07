6486391269483210fe69223a88aa369e4f899ac2
Tasnim <92645626+tasnimMoh@users.noreply.github.com>
2023-07-07 16:29:24 +0300

    Time: 7 ms (78.46%) | Memory: 44.2 MB (57.87%) - LeetSync

--- island-perimeter/island-perimeter.java
+++ island-perimeter/island-perimeter.java
@@ -1,30 +1,20 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
+        int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
-                  return  DFS(grid, i, j);  
+                  count += 4;
+                   if ( j-1 >= 0  && grid[i][j-1] == 1)  count --;
+
+                    if ( j+1 != grid[i].length && grid[i][j+1] == 1)  count --;
+
+                    if (i-1 >= 0 && grid[i-1][j] == 1)   count --;
+                    
+                    if (i+1 != grid.length && grid[i+1][j] == 1) count --;
                 }
             }
         }
-        return 0;  
-    }
-    
-    private int  DFS(int[][] grid, int i, int j) {
-        if ((i < 0 || j < 0 || i >= grid.length || j >= grid[0].length ) )
-            return 1;
-        if (grid[i][j] == 0)
-        return 1;
-        if (grid[i][j] == -1) 
-        return 0;
-        
-        int count = 0;
-        grid[i][j] = -1;
-  
-        count += DFS(grid, i + 1, j);
-        count += DFS(grid, i - 1, j);
-        count += DFS(grid, i, j + 1);
-        count += DFS(grid, i, j - 1);
-        return count;
+        return count;  
     }
 }
